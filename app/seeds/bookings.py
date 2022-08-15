5a07d6bc6e60a6f6f31ddcc0f5c8b9f607d794a7
Edward <wedward1024@gmail.com>
2022-08-14 23:12:05 -0700

    seeds

--- app/seeds/bookings.py
+++ app/seeds/bookings.py
@@ -4,36 +4,52 @@ def seed_bookings():
     booking1 = Booking(
         user_id = 2,
         listing_id = 1,
-        start_date = '2022-04-01',
-        end_date = '2022-05-01',
-        cost = 2900.00,
+        start_date = '2022-09-01',
+        end_date = '2022-10-01',
+        cost = 9000,
     )
     booking2 = Booking(
         user_id = 3,
         listing_id = 1,
-        start_date = '2022-05-02',
-        end_date = '2022-05-31',
-        cost = 3000.00,
+        start_date = '2022-08-18',
+        end_date = '2022-08-31',
+        cost = 3900,
     )
     booking3 = Booking(
         user_id = 1,
         listing_id = 2,
-        start_date = '2022-05-02',
-        end_date = '2022-05-31',
-        cost = 2800.00,
+        start_date = '2022-08-10',
+        end_date = '2022-09-05',
+        cost = 10400,
     )
     booking4 = Booking(
         user_id = 3,
         listing_id = 2,
-        start_date = '2022-06-01',
-        end_date = '2022-07-1',
-        cost = 3000.00,
+        start_date = '2022-09-07',
+        end_date = '2022-09-10',
+        cost = 1200,
+    )
+    booking5 = Booking(
+        user_id = 2,
+        listing_id = 3,
+        start_date = '2022-09-07',
+        end_date = '2022-09-10',
+        cost = 1050,
+    )
+    booking6 = Booking(
+        user_id = 1,
+        listing_id = 4,
+        start_date = '2022-09-07',
+        end_date = '2022-09-10',
+        cost = 1200,
     )
 
     db.session.add(booking1)
     db.session.add(booking2)
     db.session.add(booking3)
     db.session.add(booking4)
+    db.session.add(booking5)
+    db.session.add(booking6)
 
     db.session.commit()
 
