fc8ca92e496efaef6c4a764cf67035023edff987
Edward <wedward1024@gmail.com>
2022-09-15 15:37:55 -0700

    like route

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.listing_routes import listing_routes
 from .api.booking_route import booking_routes
-# from .api.like_routes import like_routes
+from .api.like_routes import like_routes
 
 
 from .seeds import seed_commands
@@ -37,7 +37,7 @@ app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(listing_routes, url_prefix='/api/listings')
 app.register_blueprint(booking_routes, url_prefix='/api/bookings')
-# app.register_blueprint(like_routes, url_prefix='/api/likes')
+app.register_blueprint(like_routes, url_prefix='/api/likes')
 
 
 
