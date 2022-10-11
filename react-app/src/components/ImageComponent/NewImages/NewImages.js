b0061e0a277860b93edddce9c28568e50d114894
Edward <wedward1024@gmail.com>
2022-10-10 22:44:06 -0700

    update

---
+++ react-app/src/components/ImageComponent/NewImages/NewImages.js
@@ -0,0 +1,16 @@
+// import { useEffect, useState } from 'react'
+// import { useSelector, useDispatch } from 'react-redux'
+// import { useHistory } from 'react-router-dom'
+
+
+// const NewImagePage = () => {
+//     const dispatch = useDispatch();
+//     const history = useHistory();
+
+//     const userId = useSelector(state => state.session.user.id)
+
+
+
+// }
+
+// export default NewImagePage
