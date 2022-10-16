ce4abd908aeab875697ca1fdea095e762b4cd7ff
Edward <wedward1024@gmail.com>
2022-10-15 21:19:03 -0700

    newImage frontend no store yet

--- react-app/src/components/ImageComponent/NewImages/NewImages.js
+++ react-app/src/components/ImageComponent/NewImages/NewImages.js
@@ -1,16 +1,46 @@
-// import { useEffect, useState } from 'react'
-// import { useSelector, useDispatch } from 'react-redux'
-// import { useHistory } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+// import { add}
 
 
-// const NewImagePage = () => {
-//     const dispatch = useDispatch();
-//     const history = useHistory();
+const NewImagePage = () => {
+    const dispatch = useDispatch();
+    const history = useHistory();
 
-//     const userId = useSelector(state => state.session.user.id)
+    const userId = useSelector(state => state.session.user.id)
+
+    const [image, setImage] = useState(null);
+    const [imageLoading, setImageLoading] = useState(false);
+
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+
+        const payload = {
+            image
+        }
 
 
+    }
 
-// }
+    const updateImage = (e) => {
+        const file = e.target.files[0];
+        setImage(file);
+    }
 
-// export default NewImagePage
+    return (
+        <fieldset>
+            <form onSubmit={handleSubmit}>
+                <input
+                type="file"
+                accept="image/*"
+                onChange={updateImage}
+                />
+                <button type="submit">Submit</button>
+                {/* {(imageLoading)&& <p>Loading...</p>} */}
+            </form>
+        </fieldset>
+    )
+}
+
+export default NewImagePage
